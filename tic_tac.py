a59fd7e0b216ba87c36cbc42012753ede79a6227
Koval-Nataliya <79361889+Koval-Nataliya@users.noreply.github.com>
2022-03-30 11:21:58 +0300

    Create tic_tac.py

---
+++ tic_tac.py
@@ -0,0 +1,106 @@
+import unittest
+
+board = list(range(1, 10))
+
+
+def draw_board(board):
+    for j in range(3):
+        print("|", board[0 + j * 3], "|", board[1 + j * 3], "|", board[2 + j * 3], "|")
+
+
+def start(fir):
+    if fir == 'X' or fir == 'x' or fir == 'х' or fir == 'Х':
+        return 'X'
+    elif fir == 'O' or fir == 'o' or fir == 'о' or fir == 'О' or fir == '0':
+        return 'O'
+    else:
+        return False
+
+
+class TestTT(unittest.TestCase):
+    def setUp(self):
+        print("set")
+
+    def tearDown(self):
+        print("tearD")
+
+    def testCheck_Win(self):
+        board1 = ('X', 'X', 'X', 'O', 'X', 'O')
+        self.assertEqual(check_win(board1), 'X')
+
+    def testCheck_Fail(self):
+        board2 = ('X', 'X', 'O', 'O', 'O', 'X', 'X', 'X', 'O')
+        self.assertEqual(check_win(board2), False)
+
+    def testCheck_input_incorrect(self):
+        self.assertEqual(start('p'), False)
+
+    def testCheck_input_forX(self):
+        self.assertEqual(start('x'), 'X')
+
+    def testCheck_input_forO(self):
+        self.assertEqual(start('0'), 'O')
+
+
+def take_input(player_token):
+    valid = True
+    while valid:
+        player_answer = input("Куда поставим " + player_token + "? ")
+        try:
+            player_answer = int(player_answer)
+        except:
+            print("Некорректный ввод. Введите число от 1 до 9.")
+            continue
+        if 1 <= player_answer <= 9:
+            if str(board[player_answer - 1]) not in "XO":
+                board[player_answer - 1] = player_token
+                valid = False
+            else:
+                print("Эта клетка уже занята!")
+        else:
+            print("Некорректный ввод. Введите число от 1 до 9.")
+
+
+def check_win(board):
+    win_coord = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
+    for each in win_coord:
+        if board[each[0]] == board[each[1]] == board[each[2]]:
+            return board[each[0]]
+    return False
+
+
+counter = 0
+win = False
+print("Кто начнет игру? Введите X или O.")
+i = input()
+first = start(i)
+while not first:
+    print('Некорректный ввод. Введите Х или О')
+    i = input()
+    first = start(i)
+
+if first == 'X':
+    second = 'O'
+else:
+    second = 'X'
+
+while not win:
+    draw_board(board)
+    if counter % 2 == 0:
+        take_input(first)
+    else:
+        take_input(second)
+    counter += 1
+    if counter > 4:
+        tmp = check_win(board)
+        if tmp:
+            print(tmp, "выиграл!")
+            win = True
+            break
+    if counter == 9:
+        print("Ничья!")
+        break
+draw_board(board)
+
+if __name__ == "__main__":
+    unittest.main()
